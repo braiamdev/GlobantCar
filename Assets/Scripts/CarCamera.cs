082db64d833fb238b4d0a2a0a22bb07731990c1e
braiamglobant <braiam.useche@globant.com>
2014-12-05 15:02:44 -0500

    Adding camera script

---
+++ Assets/Scripts/CarCamera.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarCamera : MonoBehaviour {
+
+	public Transform car;
+	public float distance = 6.4f;
+	public float height = 1.4f;
+	public float rotationDamping = 3.0f;
+	public float heightDamping = 2.0f;
+	public float zoomRatio = 0.5f;
+	public float reverseCamThreshold = -0.5f;
+
+	public float defaultFOV = 60;
+
+
+
+	private Vector3 rotationVector;
+
+
+	void LateUpdate () {
+	
+		float wantedAngle = rotationVector.y;
+		float wantedHeight = car.position.y + height;
+		float myAngle = transform.eulerAngles.y;
+		float myHeight = transform.position.y;
+
+
+		myAngle = Mathf.LerpAngle(myAngle, wantedAngle, rotationDamping * Time.deltaTime);
+		myHeight = Mathf.Lerp(myHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+
+		Quaternion currentRotation = Quaternion.Euler(0,myAngle,0);
+
+
+		transform.position = car.position - (currentRotation * Vector3.forward * distance);
+		transform.position = new Vector3(
+			transform.position.x,
+			myHeight,
+			transform.position.z
+		);
+
+
+		transform.LookAt(car);
+
+
+	}
+
+	void FixedUpdate(){
+		Vector3 localVelocity = car.InverseTransformDirection(car.rigidbody.velocity);
+		if(localVelocity.z < reverseCamThreshold){
+			rotationVector.y = car.eulerAngles.y + 180;
+		} else {
+			rotationVector.y = car.eulerAngles.y;
+		}
+		float acc = car.rigidbody.velocity.magnitude;
+		camera.fieldOfView = defaultFOV + acc * zoomRatio;
+	}
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+	public WheelCollider wheelFL;
+	public WheelCollider wheelFR;
+	public WheelCollider wheelRL;
+	public WheelCollider wheelRR;
+
+
+	public float maxTorque = 50f;
+	public float maxSteerAngle = 15f;
+	public Vector3 centerOfMassOffset = new Vector3(0f,-0.9f, 0f);
+
+	void Start () {
+		rigidbody.centerOfMass = new Vector3(
+			rigidbody.centerOfMass.x + centerOfMassOffset.x,
+			rigidbody.centerOfMass.y + centerOfMassOffset.y,
+			rigidbody.centerOfMass.z + centerOfMassOffset.z
+		);
+	}
+
+	void FixedUpdate () {
+		wheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
+		wheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+
+		wheelFL.steerAngle = maxSteerAngle * Input.GetAxis("Horizontal");
+		wheelFR.steerAngle = maxSteerAngle * Input.GetAxis("Horizontal");
+
+	}
+}
